2fe0bf09f759a1100a193fded7345994ff01160d
Horia Radu <horia.radu23@gmail.com>
2016-06-03 16:11:29 +0300

    try to use stateless-functional-components
    http://thereignn.ghost.io/favour-stateless-functional-components-over-es6-classes/

--- client/components/Tweet.js
+++ client/components/Tweet.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import {Panel, Col, Image} from 'react-bootstrap';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-class Tweet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-  }
+const Tweet = ({tweet}) => {
+  return (
+    <Panel>
+      <div className="tweet-content">
+        <Image src={tweet.getIn(['user', 'profile_image_url'])} circle className="tweet-user-img"/>
 
-  render() {
-    return (
-      <Panel>
-        <div className="tweet-content">
-          <Image src={this.props.tweet.getIn(['user', 'profile_image_url'])} circle className="tweet-user-img"/>
-
-          <div>
-            <strong>{this.props.tweet.getIn(['user', 'name'])}</strong>
-          </div>
-          <div className="twitter-handle">
-            <span>@{this.props.tweet.getIn(['user', 'screen_name'])}</span>
-          </div>
-
-          <div>
-            {this.props.tweet.get('text')}
-          </div>
-
-          <div className="tweet-time">
-            {this.props.tweet.get('created_at')}
-          </div>
+        <div>
+          <strong>{tweet.getIn(['user', 'name'])}</strong>
         </div>
-      </Panel>
-    );
-  }
-}
+        <div className="twitter-handle">
+          <span>@{tweet.getIn(['user', 'screen_name'])}</span>
+        </div>
+
+        <div>
+          {tweet.get('text')}
+        </div>
+
+        <div className="tweet-time">
+          {tweet.get('created_at')}
+        </div>
+      </div>
+    </Panel>
+  );
+};
 
 export default Tweet;
